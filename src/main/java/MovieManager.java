95840e278f3eaee57449d6caf7aa630a94cf39e4
Elizabeth Viricheva <liza.vir@ya.ru>
2024-02-02 14:39:12 +0300

    ver1

---
+++ src/main/java/MovieManager.java
@@ -0,0 +1,9 @@
+public class MovieManager {
+    private String movieName;
+    public movieManager(String movieName) {
+        this.movieName = movieName;
+    }
+    private MovieManager[] movie = new MovieManager[0];
+
+    get set
+}
