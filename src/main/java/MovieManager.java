6f9c136f18e6fbd722545396178060551a710e5d
Elizabeth Viricheva <liza.vir@ya.ru>
2024-02-04 11:17:54 +0300

    tests and code

--- src/main/java/MovieManager.java
+++ src/main/java/MovieManager.java
@@ -1,9 +1,44 @@
 public class MovieManager {
-    private String movieName;
-    public movieManager(String movieName) {
-        this.movieName = movieName;
-    }
-    private MovieManager[] movie = new MovieManager[0];
+    private String[] movies = new String[0]; //все фильмы должны храниться внутри массива в поле самого менеджера
+    private int amount;
 
-    get set
+    public MovieManager() { //constr
+        this.amount = 5;
+    }
+
+    public MovieManager(int amount) {
+        this.amount = amount;
+    }
+
+    public void add(String movie) {
+        String[] tmp = new String[movies.length + 1];
+
+        for (int i = 0; i < movies.length; i++) {
+            tmp[i] = movies[i];
+        }
+        tmp[tmp.length - 1] = movie; //заполнить последнюю ячейку
+        movies = tmp;
+    }//добавляет фильм в массив
+
+    public String[] findAll() {
+        String[] reversed = new String[movies.length];
+        for (int i = 0; i < reversed.length; i++) {
+            reversed[i] = movies[movies.length - 1 - i];
+        }
+        return reversed;
+    }//выведет массив в порядке добавления
+
+    public String[] findLast() {
+        int resultLength;
+        if (movies.length < amount) {
+            resultLength = movies.length;
+        } else {
+            resultLength = amount;
+        }
+        String[] result = new String[resultLength];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = movies[movies.length - 1 - i];
+        }
+        return result;
+    }//последние фильмы в массиве
 }
--- src/test/java/MovieManagerTest.java
+++ src/test/java/MovieManagerTest.java
@@ -1,3 +1,48 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MovieManagerTest {
+
+
+    @Test
+    public void shouldAddMoviesAndGetAllReversed() {
+        MovieManager manager = new MovieManager();
+        manager.add("Джентельмены");
+        manager.add("Шрек");
+        String[] expected = {"Шрек", "Джентельмены"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetDefaultLimit() {
+        MovieManager manager = new MovieManager();
+        manager.add("Джентельмены");
+        manager.add("Шрек");
+        manager.add("Шрек 2");
+        manager.add("Шрек 3");
+        manager.add("Шрек 4");
+        manager.add("Кот в сапогах");
+
+        String[] expected = {"Кот в сапогах", "Шрек 4", "Шрек 3", "Шрек 2", "Шрек"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetLimitAndGetArrLessThanLimit() {
+        MovieManager manager = new MovieManager(8);
+        manager.add("Джентельмены");
+        manager.add("Шрек");
+        manager.add("Шрек 2");
+        manager.add("Шрек 3");
+        manager.add("Шрек 4");
+        manager.add("Кот в сапогах");
+
+        String[] expected = {"Кот в сапогах", "Шрек 4", "Шрек 3", "Шрек 2", "Шрек", "Джентельмены"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
 
 }
